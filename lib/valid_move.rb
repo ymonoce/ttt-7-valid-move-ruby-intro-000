7f98f24fde93ca1d4dbb2f49f91af98b5fec0bf9
Chloe Atwood <atwoodksw@gmail.com>
2020-12-04 22:33:42 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-board[index].between?(0,8)
+index.between?(0,8) && position_taken?
+
 end
 
 # define method with parameters board array and index number on board array
